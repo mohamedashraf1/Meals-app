a6cd684d2438ee3364a47c3eada656e96101edfb
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2020-12-09 01:37:25 +0200

    moved the tabs to the bottom of the screen

--- MealsApp/lib/screens/tabs_screen.dart
+++ MealsApp/lib/screens/tabs_screen.dart
@@ -9,9 +9,63 @@ class TabsSreen extends StatefulWidget {
 }
 
 class _TabsSreenState extends State<TabsSreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoriteScreen(),
+      'title': 'Favorites',
+    },
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _pages[_selectedPageIndex]['title'],
+        ),
+      ),
+      drawer: Drawer(
+        child: Text("The drawer!"),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        items: [
+          BottomNavigationBarItem(
+            //backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        //type: BottomNavigationBarType.shifting,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}
+
+/// adding tabs from the appbar
+/*
+ DefaultTabController(
       //initialIndex: 0,
       child: Scaffold(
         appBar: AppBar(
@@ -38,5 +92,5 @@ class _TabsSreenState extends State<TabsSreen> {
       ),
       length: 2,
     );
-  }
-}
+
+ */
